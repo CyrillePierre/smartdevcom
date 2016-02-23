7613bd5491bcf010b262caacc58b13d7d4a7bf68
Cyrille <cypierre58@gmail.com>
2016-02-23 01:00:56 +0100

    Réponse des requêtes VARP opérationnel.

--- protocol/src/net/netinterpreter.cpp
+++ protocol/src/net/netinterpreter.cpp
@@ -35,106 +35,41 @@ void NetInterpreter::manageVARP(NetStream & ns) const {
     header.op      = ns.read<type::Byte>(3);
     header.scale   = ns.read<type::Byte>(5);
 
-    ns.read(header.scAddrSrc,  cSize);
+    ns.read(header.scAddrSrc,  header.scale);
     ns.read(header.addrSrc,    vSize);
-    ns.read(header.scAddrDest, cSize);
+    ns.read(header.scAddrDest, header.scale);
     ns.read(header.addrDest,   vSize);
 
-    {
-        std::size_t i;
-        std::cout << std::hex;
-        std::cout << "comAddrSrc = ";
-        for (i = 0; i < cSize - 1; ++i)
-            std::cout << (int) header.scAddrSrc[i] << ":";
-        std::cout << (int) header.scAddrSrc[i] << std::endl;
-    }
-
-    Addr scAddrDest{header.scAddrDest, cSize},
+    Addr scAddrDest{header.scAddrDest, header.scale},
          addrDest{header.addrDest, vSize};
-//    if (header.op == COM_ADDR_REQUEST && )
 
+    if ((header.op == COM_ADDR_REQUEST && nd.virtualAddr().accept(addrDest))
+         || (header.op == VIP_ADDR_REQUEST && nd.comAddr().accept(scAddrDest)))
+        sendVARP(ns, header);
 }
-
-#if 0
-/**
- * @brief Cette méthode permer de traiter les paquets de type VARP
- * @param ns : la trame reçue
- */
-void NetInterpreter::manageVARP(NetStream &ns) const {
-    NetDevice const & device = ns.device();
-    uint16_t const cSize = device.comAddrSize(),
-                   vSize = NetDevice::virtualAddrSize;
-
-    VARPHeader  readingVarp(cSize), writingVarp(cSize);
-    type::Byte  byte;
-
-    ns.read(byte, 3);
-    readingVarp.version = byte;
-
-    ns.read(byte, 3);
-    readingVarp.op = byte;
-
-    ns.read(byte, 5);
-    readingVarp.scale = byte;
-
-    ns.read(readingVarp.scAddrSrc,  cSize);
-    ns.read(readingVarp.addrSrc,    vSize);
-    ns.read(readingVarp.scAddrDest, cSize);
-    ns.read(readingVarp.addrDest,   vSize);
-
-    {
-        int i;
-        std::cout << std::hex;
-        std::cout << "comAddrSrc = ";
-        for (i = 0; i < cSize - 1; ++i)
-            std::cout << (int) readingVarp.scAddrSrc[i] << ":";
-        std::cout << (int) readingVarp.scAddrSrc[i] << std::endl;
-    }
-
-    /* Si l'adresse de destination dans le support ou dans le réseau virtuel,
-     * c'est moi ou si c'est du broadcast, et que c'est une demande d'adresse */
-    if((memcmp(readingVarp.scAddrDest, device.getComAddr(), readingVarp.scale)  ||
-        memcmp(readingVarp.addrDest, device.getVirtualAddr(), vSize)     ||
-        memcmp(readingVarp.scAddrDest, NetDevice::BROADCAST, readingVarp.scale) ||
-        memcmp(readingVarp.addrDest, NetDevice::BROADCAST, vSize))       &&
-       (readingVarp.op == 0x01 || readingVarp.op == 0x00))
-    {
-        writingVarp.version = readingVarp.version;
-        writingVarp.scale   = readingVarp.scale;
-        writingVarp.op      = 0x02;
-
-        memcpy((char*) writingVarp.scAddrSrc, (char*) device.getComAddr(), cSize);
-        memcpy((char*) writingVarp.addrSrc, (char*) device.getVirtualAddr(), vSize);
-        memcpy((char*) writingVarp.scAddrDest, (char*) readingVarp.scAddrSrc, cSize);
-        memcpy((char*) writingVarp.addrDest, (char*) readingVarp.addrSrc, vSize);
-
-        sendVARP(ns, writingVarp);
-    }
-}
-
-#endif
 
 /**
  * @brief Cette méthode permet d'envoyer un header VARP
  * @param ns : le stream sur lequel envoyer les données
- * @param header : la trame à envoyer
+ * @param header : la trame reçue à modifier pour l'envoi
  */
 void NetInterpreter::sendVARP(NetStream &ns, const VARPHeader &header) const
 {
-    DynamicBitset dbs = ns.writingBitset();
-    uint16_t const cSize = ns.device().comAddrSize(),
-                   vSize = NetDevice::virtualAddrSize;
+    DynamicBitset & dbs = ns.writingBitset();
 
-    dbs.push(header.version);
-    dbs.push(header.scale);
-    dbs.push(header.op);
+    dbs.clear();
+    dbs.push((type::Byte) Proto::VARP, 5);
+    dbs.push(header.version,           3);
+    dbs.push((type::Byte) RESPONSE,    3);
+    dbs.push(header.scale,             5);
 
-    std::cout << dbs.data();
+    NetDevice const & nd = ns.device();
+    Addr const & comAddr = nd.comAddr(), addr = nd.virtualAddr();
 
-    dbs.push(header.scAddrSrc,  cSize);
-    dbs.push(header.addrSrc,    vSize);
-    dbs.push(header.scAddrDest, cSize);
-    dbs.push(header.addrDest,   vSize);
+    dbs.push(comAddr.vals, comAddr.size);
+    dbs.push(addr.vals, addr.size);
+    dbs.push(header.scAddrSrc, comAddr.size);
+    dbs.push(header.addrSrc, addr.size);
 
     ns.flushOut();
 }
--- protocol/test/testnetinterpreter/testnetinterpreter.cpp
+++ protocol/test/testnetinterpreter/testnetinterpreter.cpp
@@ -8,23 +8,31 @@
 using namespace sdc;
 
 void TestNetInterpreter::vipAddrRequests() {
-    type::Byte deviceBuf[32] =
-        {0x08, 0x04,
-         0xc0, 0xa7, 0x1c, 0x04, 	0x1b, 0xc7, 0x03,
-         0xff, 0xff, 0xff, 0xff,	0x1b, 0xc7, 0x05};
-
-    test::FakeDevice fd(net::makeAddr<0xc0, 0xa7, 0x1c, 0x04>(),
-                        net::makeAddr<0x1b, 0xc7, 0x03>());
-    net::NetStream ns(fd);
+    test::FakeDevice fd(net::makeAddr<0xc0, 0xa7, 0xd9, 0x78>(),
+                        net::makeAddr<0x1b, 0xc7, 0x05>());
     vnet::SDCPInterpreter sdcp;
     vnet::VIPInterpreter vip(sdcp);
     net::NetInterpreter net(vip);
-    char *hex;
 
-    fd.buf = deviceBuf;
-    net(ns);
-    hex = QTest::toHexRepresentation((char *) deviceBuf, 16);
-    QCOMPARE(hex, "00 32 B0 B1 B2 A0 A1 A2 00 01 01");
+    auto callTest = [&net, &fd] (type::Byte * buf, int size, char const * str) {
+        fd.reset();
+        fd.buf = buf;
+        net::NetStream ns(fd);
+        net(ns);
+        char *hex = QTest::toHexRepresentation((char *) buf, size);
+        QCOMPARE(hex, str);
+    };
+
+    type::Byte buf1[32] = {0x08, 0x04,
+                           0xc0, 0xa7, 0x1c, 0x04, 	0x1b, 0xc7, 0x03,
+                           0xff, 0xff, 0xff, 0xff,	0x1b, 0xc7, 0x05};
+
+    type::Byte buf2[32] = {0x08, 0x24,
+                           0xc0, 0xa7, 0x1c, 0x04, 	0x1b, 0xc7, 0x03,
+                           0xc0, 0xa7, 0xd9, 0x78,	0x1b, 0xff, 0xff};
+
+    callTest(buf1, 16, "08 44 C0 A7 D9 78 1B C7 05 C0 A7 1C 04 1B C7 03");
+    callTest(buf2, 16, "08 44 C0 A7 D9 78 1B C7 05 C0 A7 1C 04 1B C7 03");
 }
 
 void TestNetInterpreter::testVarp() {
